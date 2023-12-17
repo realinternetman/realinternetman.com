61fae6ea3a66be5ed7bd3ed7c1239bb2b9e188bf
realinternetman <realinternetman@gmail.com>
2023-12-18 02:26:57 +0900

    feat: add decap cms

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  webpack: (cfg) => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    });
+    return cfg;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
